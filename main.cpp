a1736445015f9ea2ca572a34cf2076656341e460
aisyahak <1211101007@student.mmu.edu.my>
2023-01-25 10:43:45 +0800

    changes to board and game settings

--- main.cpp
+++ main.cpp
@@ -5,23 +5,128 @@
 // Names: AISYAH BINTI AHMAD KASSIM | ALIA MAISARA BINTI SHAHRIN | AZRYL SHAMIN BIN AZRIZAL 
 // IDs: 1211101007 | 1211104237 | 1211103145 
 // Emails: 1211101007@student.mmu.edu.my | 1211104237@student.mmu.edu.my | 1211103145@student.mmu.edu.my 
-// Phones: 0197345509 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
+// Phones: 0197345509 | MEMBER_PHONE_2 | 01110169553 
 // ********************************************************* 
 
 // TODO: Fill in the missing information above and delete this line.
 
 #include "pf/helper.h"
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
 using namespace std;
+
+void pause();
+void clearScreen();
+void createGameBoard();
+void displayMenu();
+
 
 int main()
 {
     cout << "Assignment (Part 1)" << endl;
     cout << "Let's Get Started!" << endl;
-    pf::Pause();
+
+    char choice;
+    int numOfRows{3}, numOfColumns {19}, numOfZombies {1};
+    bool done {0};
+
+    displayMenu();
+    cin >> choice;
+    switch (choice)
+    {
+        case '1':
+            cout << "Option 1 not yet implemented." << endl;
+            break;
+        case '2':
+            cout << "Option 2 Not yet implemented." << endl;
+            break;
+        case '3':
+            cout << "Option 3 Not yet implemented." << endl;
+            break;
+        case 'Q':
+            done = true;
+            break;
+        default:
+            cout << "Invalid selection, try again!" << endl;
+            cout << endl;
+            break;
+    }
+    system("pause");
+    // CreateGameBoard();
 }
 
-//test
+void pause()
+{
+    cout << "Pausing Now" << endl;
+    pf::Pause();
+    cout << endl;
+}
+
+void clearScreen()
+{
+    cout << "Pausing and Clearing Screen Now" << endl;
+    pf::Pause();
+    pf::ClearScreen();
+    cout << endl;
+}
+
+void displayMenu()
+{
+    system("cls");
+    cout << "+---------------------------------------------+" << endl;
+    cout << "|               ALIEN VS ZOMBIE               |" << endl;
+    cout << "|=============================================|" << endl;
+    cout << "| Select:                                     |" << endl;
+    cout << "|    1 => New Game                            |" << endl;
+    cout << "|    2 => Load Game                           |" << endl;
+    cout << "|---------------------------------------------|" << endl;
+    cout << "|    3 => Settings                            |" << endl;
+    cout << "|---------------------------------------------|" << endl;
+    cout << "|    Q => Quit                                |" << endl;
+    cout << "+---------------------------------------------+" << endl;
+    cout << endl;
+    cout << "Choice => ";
+}
+
+void createGameBoard()
+{
+
+}
+
+
+
+void showGameSettings(const int &rows, const int &columns, const int &zombies){
+
+    cout << "Game Settings\n";
+    cout << "----------------\n";
+
+    cout << "Board Rows : " << rows << "\n";
+    cout << "Board Columns : " << columns << "\n";
+    cout << "Zombie Count : " << zombies << "\n";
+
+    char changeSettings;
+
+    cout << "Do you wish to change the game settings? (y/n) => ";
+    cin >> changeSettings;
+    switch (changeSettings)
+    {
+    case 'y':
+        cout << "not yet implement ";
+        break;
+    case 'n':
+        cout << "TO DO";
+        break;
+    default:
+        cout << "Invalid selection, try again!";
+    }
+
+}
 
 //cuba check guys
 #include <iostream>
