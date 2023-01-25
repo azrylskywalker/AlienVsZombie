3463b49e1e8eb1b09d03dcaf0f93c8ef0a7b2fca
azrylskywalker <azyyshm@gmail.com>
2023-01-25 10:40:20 +0800

    compile

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS
 // Year: Trimester 1, 2022/23 (T2215)
 // Lab: TT8L
-// Names: AISYAH BINTI AHMAD KASSIM | MEMBER_NAME_2 | AZRYL SHAMIN BIN AZRIZAL
-// IDs: MEMBER_ID_1 | MEMBER_ID_2 | 1211103145
-// Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | 1211103145@student.mmu.edu.my
-// Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | 01110169553
+// Names: AISYAH BINTI AHMAD KASSIM | ALIA MAISARA BINTI SHAHRIN | AZRYL SHAMIN BIN AZRIZAL
+// IDs: 1211101007 | 1211104237 | 1211103145
+// Emails: 1211101007@student.mmu.edu.my | 1211104237@student.mmu.edu.my | 1211103145@student.mmu.edu.my
+// Phones: 0197345509  | MEMBER_PHONE_2 | 01110169553
 // *********************************************************
 
 #include "pf/helper.h"
 #include <iostream>
+#include <vector>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,7 +19,115 @@ void pause();
 void clearScreen();
 void createGameBoard();
 void displayMenu();
+void showGameSettings();
 
+class Board
+{
+private:
+    vector<vector<char>> boardsize_;
+    int row_, column_;
+
+public:
+    Board(int boardColumns = 5, int boardRows = 10);
+    void init(int boardColumns, int boardRows);
+    void display() const;
+};
+
+Board::Board(int boardColumns, int boardRows)
+{
+    cout << endl;
+    cout << "Board Settings" << endl;
+    cout << "----------------" << endl;
+
+    cout << "Enter rows => ";
+    cin >> boardRows;
+    cout << "Enter columns => ";
+    cin >> boardColumns;
+
+    init(boardColumns, boardRows);
+}
+
+void Board::init(int boardColumns, int boardRows)
+{
+    row_ = boardRows;
+    column_ = boardColumns;
+
+    char objects[] = {'h', 'p', 'r', '>', '<', '^', 'v', ' ', ' ', ' '};
+    int noOfObjects = 10;
+
+    boardsize_.resize(row_);
+    for (int i = 0; i < row_; ++i)
+    {
+        boardsize_[i].resize(column_);
+    }
+
+    for (int i = 0; i < row_; ++i)
+    {
+        for (int j = 0; j < column_; ++j)
+        {
+            int objNo = rand() % noOfObjects;
+            boardsize_[i][j] = objects[objNo];
+        }
+    }
+}
+
+void Board::display() const
+{
+
+    // system("cls");
+    cout << setw(15);
+    cout << " .: Alien vs Zombie :." << endl;
+
+    for (int i = 0; i < row_; ++i)
+    {
+        cout << "   ";
+        for (int j = 0; j < column_; ++j)
+        {
+            cout << "+-";
+        }
+        cout << "+" << endl;
+
+        cout << setw(2) << (row_ - i) << " ";
+
+        for (int j = 0; j < column_; ++j)
+        {
+            cout << "|" << boardsize_[i][j];
+        }
+        cout << "|" << endl;
+    }
+
+    cout << "   ";
+    for (int j = 0; j < column_; ++j)
+    {
+        cout << "+-";
+    }
+    cout << "+" << endl;
+
+    cout << "   ";
+    for (int j = 0; j < column_; ++j)
+    {
+        int digit = (j + 1) / 10;
+        cout << " ";
+        if (digit == 0)
+            cout << " ";
+        else
+            cout << digit;
+    }
+    cout << endl;
+    cout << "   ";
+    for (int j = 0; j < column_; ++j)
+    {
+        cout << " " << (j + 1) % 10;
+    }
+    cout << endl
+         << endl;
+}
+
+void gameBoardDisplay()
+{
+    Board board;
+    board.display();
+}
 
 int main()
 {
@@ -25,47 +135,32 @@ int main()
     cout << "Let's Get Started!" << endl;
 
     char choice;
-    int numOfRows{3}, numOfColumns {19}, numOfZombies {1};
-    bool done {0};
+    int numOfRows{3}, numOfColumns{19}, numOfZombies{1};
+    bool done{0};
 
     displayMenu();
     cin >> choice;
     switch (choice)
     {
-        case '1':
-            cout << "Option 1 not yet implemented." << endl;
-            break;
-        case '2':
-            cout << "Option 2 Not yet implemented." << endl;
-            break;
-        case '3':
-            cout << "Option 3 Not yet implemented." << endl;
-            break;
-        case 'Q':
-            done = true;
-            break;
-        default:
-            cout << "Invalid selection, try again!" << endl;
-            cout << endl;
-            break;
+    case '1':
+        cout << "Option 1 not yet implemented." << endl;
+        break;
+    case '2':
+        cout << "Option 2 Not yet implemented." << endl;
+        break;
+    case '3':
+        cout << "Option 3 Not yet implemented." << endl;
+        break;
+    case 'Q':
+        done = true;
+        break;
+    default:
+        cout << "Invalid selection, try again!" << endl;
+        cout << endl;
+        break;
     }
     system("pause");
     // CreateGameBoard();
-}
-
-void pause()
-{
-    cout << "Pausing Now" << endl;
-    pf::Pause();
-    cout << endl;
-}
-
-void clearScreen()
-{
-    cout << "Pausing and Clearing Screen Now" << endl;
-    pf::Pause();
-    pf::ClearScreen();
-    cout << endl;
 }
 
 void displayMenu()
@@ -88,16 +183,14 @@ void displayMenu()
 
 void createGameBoard()
 {
-
 }
 
-
-
-void showGameSettings(const int &rows, const int &columns, const int &zombies){
+void showGameSettings(const int &rows, const int &columns, const int &zombies)
+{
 
     cout << "Game Settings\n";
     cout << "----------------\n";
-    
+
     cout << "Board Rows : " << rows << "\n";
     cout << "Board Columns : " << columns << "\n";
     cout << "Zombie Count : " << zombies << "\n";
@@ -117,5 +210,8 @@ void showGameSettings(const int &rows, const int &columns, const int &zombies){
     default:
         cout << "Invalid selection, try again!";
     }
+}
+
+void changeGameSettings(){
 
 }
